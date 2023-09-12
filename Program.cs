b9cce43f07712435f46b5f329998e1d0a4a83757
daniel2005d <daniel2005d@gmail.com>
2023-09-11 19:08:39 -0700

    Add dot lines 4 separate each column and each value

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 
 namespace SqlClient
 {
@@ -11,11 +12,11 @@ namespace SqlClient
         static void Main(string[] args)
         {
             string connString = string.Empty;
-            
+
 
             ArgOptions options = CommandLineArguments.ArgParse(args);
             bool error = false;
-            
+
             if (string.IsNullOrEmpty(options.DatabaseName))
             {
                 error = true;
@@ -46,44 +47,29 @@ namespace SqlClient
             }
 
             string query = options.Query;
-
-
+            List<int> columnWidths = new List<int>();
+            
             try
             {
                 using (SqlConnection conn = new SqlConnection(connString))
                 {
                     //retrieve the SQL Server instance version
-                    
 
+                    DataSet ds = new DataSet();
                     SqlCommand cmd = new SqlCommand(query, conn);
-
-                    //open connection
-                    conn.Open();
-
-                    //execute the SQLCommand
-                    SqlDataReader dr = cmd.ExecuteReader();
-
-                    //check if there are records
-                    if (dr.HasRows)
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+                    if (ds.Tables.Count > 0)
                     {
-                        while (dr.Read())
-                        {
-                            //display retrieved record (first column only/string value)
-                            for (int i = 0; i < dr.FieldCount; i++)
-                            {
-                                Console.WriteLine(dr.GetName(i));
-                            }
-                            for (int i = 0; i < dr.FieldCount; i++)
-                            {
-                                Console.WriteLine(dr.GetValue(i));
-                            }
-                        }
+                        PrintDataTable(ds.Tables[0]);
                     }
+
+                    
                     else
                     {
                         Console.WriteLine("No data found.");
                     }
-                    dr.Close();
+                    
                 }
             }
             catch (Exception ex)
@@ -91,6 +77,53 @@ namespace SqlClient
                 //display error message
                 Console.WriteLine("Exception: " + ex.Message);
             }
+        }
+
+        static void PrintDataTable(DataTable dataTable)
+        {
+            string[] columnNames = dataTable.Columns.Cast<DataColumn>().Select(column => column.ColumnName).ToArray();
+
+            // Calculate the minimum width for each column
+            int[] columnWidths = new int[dataTable.Columns.Count];
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                int maxColumnWidth = columnNames[i].Length;
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    string value = row[i].ToString();
+                    if (value.Length > maxColumnWidth)
+                    {
+                        maxColumnWidth = value.Length;
+                    }
+                }
+                columnWidths[i] = maxColumnWidth + 2; // Add margin
+            }
+
+            // Print the table
+            Console.WriteLine(new string('=', columnWidths.Sum() + columnWidths.Length - 1));
+            foreach (var columnName in columnNames)
+            {
+                int itemIndex = columnNames.ToList().IndexOf(columnName);
+                string formattedItem = columnName.PadRight(columnWidths[itemIndex]);
+                Console.Write($"| {formattedItem} ");
+                
+            }
+
+            Console.WriteLine("|");
+            Console.WriteLine(new string('=', columnWidths.Sum() + columnWidths.Length - 1));
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    int itemIndex = row.ItemArray.ToList().IndexOf(item);
+                    string formattedItem = item.ToString().PadRight(columnWidths[itemIndex]);
+                    Console.Write($"| {formattedItem} ");
+                }
+                Console.WriteLine("|");
+            }
+
+            Console.WriteLine(new string('=', columnWidths.Sum() + columnWidths.Length - 1));
         }
     }
 }
